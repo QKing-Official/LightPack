2fa1cc0746509fc58d151f0b84245ae126881975
QKing <162024807+QKing-Official@users.noreply.github.com>
2025-02-17 20:33:57 +0100

    Update hello-world.py

--- packages/hello-world/hello-world.py
+++ packages/hello-world/hello-world.py
@@ -1,8 +1,8 @@
-# hello_world.py (this script is the package)
+# hello_world.py
 
 def run_hello_world():
     print("Hello, World!")
-    print("This is the Hello World package installation.")
+    print("This is the Hello World package.")
 
 if __name__ == "__main__":
     run_hello_world()
