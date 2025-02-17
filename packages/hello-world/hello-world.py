cabe46d3b7f65b03f5a8715b32e9e0b639f97ae8
QKing <162024807+QKing-Official@users.noreply.github.com>
2025-02-17 20:31:45 +0100

    Update and rename install.py to hello-world.py

---
+++ packages/hello-world/hello-world.py
@@ -0,0 +1,8 @@
+# hello_world.py (this script is the package)
+
+def run_hello_world():
+    print("Hello, World!")
+    print("This is the Hello World package installation.")
+
+if __name__ == "__main__":
+    run_hello_world()
